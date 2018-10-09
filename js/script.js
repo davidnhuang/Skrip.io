b74b4c188b2ca13b8e955db0f937f698adbe945f
David <David.N.Huang@gmail.com>
2018-10-08 22:00:58 -0700

    Prototyped chat feed - starting file feed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-$(function() {});
+$(document).ready(function(){
+
+  var height = 0;
+  $("#feed-box .meeting-feed-listing").each(function(i, value){
+      height += parseInt($(this).height());
+    });
+
+  height += '';
+
+  $("#feed-box").animate({scrollTop: height});
+});
