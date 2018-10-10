c8d1c31089d975f622b76d9a700c04d3d6f59181
David <David.N.Huang@gmail.com>
2018-10-09 22:24:05 -0700

    Integrated context menu

--- js/script.js
+++ js/script.js
@@ -64,4 +64,49 @@ $(document).ready(function(){
   $(document).keyup(function(e) {
     if (e.keyCode === 27) $('#overlay').fadeOut(250);
   });
+
+  //Context menues
+  // Scheduler Menus
+  // Trigger action when the contexmenu is about to be shown
+  $("#meeting-confirmation-option").click("contextmenu", function (event) {
+
+      // Avoid the real one
+      event.preventDefault();
+
+      // Show contextmenu
+      $(".meeting-post-options").finish().toggle(100).
+
+      // In the right position (the mouse)
+      css({
+        top: event.pageY + "px",
+        left: (event.pageX -= 120) + "px"
+      });
+  });
+
+  // If the document is clicked somewhere
+  $(document).bind("mousedown", function (e) {
+
+      // If the clicked element is not the menu
+      if (!$(e.target).parents(".custom-menu").length > 0) {
+          // Hide it
+          $(".meeting-post-options").hide(100);
+      }
+  });
+
+
+  // If the menu element is clicked
+  $(".meeting-post-options li").click(function(){
+
+      // This is the triggered action name
+      switch($(this).attr("data-action")) {
+
+          // A case for each action. Your actions here
+          case "first": alert("first"); break;
+          case "second": alert("second"); break;
+          case "third": alert("third"); break;
+      }
+
+      // Hide it AFTER the action was triggered
+      $(".custom-menu").hide(100);
+    });
 });
