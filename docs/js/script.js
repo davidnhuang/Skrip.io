32a7bc8b98cb5404f1628d6647ce693571102291
David <David.N.Huang@gmail.com>
2018-10-30 20:07:38 -0700

    Still tweaking animation delay

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   setTimeout(function(){
     $("#loading-screen").fadeOut(100)
     $("#target-content").addClass("fadingIn")
-  },800);
+  },400);
 
   $(".loading-bar").animate({width: "100%"});
 
